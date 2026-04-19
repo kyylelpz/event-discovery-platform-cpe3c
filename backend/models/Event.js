747b8414e0b362650cceb3d028b25a9ffd7cd2d5
CPE3C-aquino-reymel <2023103201@ms.bulsu.edu.ph>
2026-04-19 11:00:45 +0800

    Add Event and Venue models

---
+++ backend/models/Event.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const EventSchema = new mongoose.Schema({
+
+    title: { type: String, required: true },
+    description: { type: String },
+    category: { type: String },
+    venue_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Venue' },
+    start_date: { type: Date, required: true },
+    end_date: { type: Date, required: true },
+    api_source: { type: String }, 
+    image: { type: String },     
+    tickets_info: { type: String } 
+}, { timestamps: true }); 
+
+module.exports = mongoose.model('Event', EventSchema);
