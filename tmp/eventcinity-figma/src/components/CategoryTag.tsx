8ad3590c9ded07f77d66718a1bae4da90468217b
kyylelpz <lopez.kyle922@gmail.com>
2026-04-23 18:56:24 +0800

    Clean repo

--- tmp/eventcinity-figma/src/components/CategoryTag.tsx
+++
@@ -1,7 +0,0 @@
-export function CategoryTag({ category }) {
-  return (
-    <span className="inline-block px-2 sm:px-3 py-1 text-xs sm:text-sm bg-[#2D3B15] text-[#FCFCFC] rounded-full">
-      {category}
-    </span>
-  );
-}
